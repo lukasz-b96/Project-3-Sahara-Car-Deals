a88a3c63e13993b8cab8f994c151bb7f25026e4a
Lukasz <freshbenypl@gmail.com>
2022-01-24 11:51:48 +0100

    front end almost finished

--- server/controllers/carController.js
+++ server/controllers/carController.js
@@ -1,6 +1,47 @@
+require("../models/database");
+const Brand = require("../models/brand");
+
 // GET /
 // HomePage
 
 exports.homepage = async (req, res) => {
-  res.render("index", { title: "CarDeals - Home" });
+  try {
+    const limit = 5;
+    const brands = await Brand.find({}).limit(limit);
+
+    res.render("index", { title: "CarDeals - Home", brands });
+  } catch (error) {
+    res.status(500).send({ message: error.message || "error occur" });
+  }
 };
+
+// async function insertDummyCategoryData() {
+//   try {
+//     await Category.insertMany([
+//       {
+//         name: "Pagani",
+//         image: "Pagani.png",
+//       },
+//       {
+//         name: "Bentley",
+//         image: "Bentley.png",
+//       },
+//       {
+//         name: "Jaguar",
+//         image: "Jaguar.png",
+//       },
+//       {
+//         name: "Land Rover",
+//         image: "LandRover.png",
+//       },
+//       {
+//         name: "McLaren",
+//         image: "McLaren.png",
+//       },
+//     ]);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// insertDummyCategoryData();
